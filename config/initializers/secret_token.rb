f1afa89a938df9c9c6579a2acd979dada8a89747
Chase Stubblefield <chasestubblefield@gmail.com>
2013-03-14 23:47:40 -0700

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Higgins::Application.config.secret_key_base = 'a5e772d4cc8284ef05ca3c8b97bed87050a63a5589be0be7f3cab08fed81ab9255613038dc25fca660521de4809ff5703e079e762c1afc9200e70c5ed98d82d6'
